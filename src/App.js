14abb4ff07a82193ce49c9e138c477afbe4911f5
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2018-02-27 13:35:59 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import CartHeader from './components/CartHeader'
+import CartFooter from './components/CartFooter'
+import CartItems from './components/CartItems'
+import AddItem from './components/AddItem'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
